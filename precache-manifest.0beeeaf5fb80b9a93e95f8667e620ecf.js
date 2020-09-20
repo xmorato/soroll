3f96f080571437161b097ce0bdaf7240274a0bc3
Xavi Morató <xmorato@gmail.com>
2020-09-21 00:02:48 +0200

    Updates

---
+++ precache-manifest.0beeeaf5fb80b9a93e95f8667e620ecf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c38903dc8446fce0f56eed6f68cc319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a3153e14abf7f03d409",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "2a6c68694b835913f7d2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a3153e14abf7f03d409",
+    "url": "/static/js/2.3124a88f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3124a88f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6c68694b835913f7d2",
+    "url": "/static/js/main.46f43cda.chunk.js"
+  },
+  {
+    "revision": "39ba5c5bf97fe9656f14",
+    "url": "/static/js/runtime-main.738c5e2f.js"
+  }
+]);
