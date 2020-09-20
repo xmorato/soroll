b612085ca4a4bc38a247b7eedf285d8c760e7eb6
Xavi Morató <xmorato@gmail.com>
2020-09-21 00:40:24 +0200

    Updates

---
+++ precache-manifest.9293e4c7b253c8020c305700228e554f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62df5680636d2c8301cade758ddf101e",
+    "url": "/soroll/index.html"
+  },
+  {
+    "revision": "b076e212eabebd85d32b",
+    "url": "/soroll/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "50697996144827a57149",
+    "url": "/soroll/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b076e212eabebd85d32b",
+    "url": "/soroll/static/js/2.fcc59246.chunk.js"
+  },
+  {
+    "revision": "7d2d2ecba9f0218337fe9aaf2b5b742e",
+    "url": "/soroll/static/js/2.fcc59246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50697996144827a57149",
+    "url": "/soroll/static/js/main.fe81d9d7.chunk.js"
+  },
+  {
+    "revision": "366dca43ed05a996ef33",
+    "url": "/soroll/static/js/runtime-main.84642ba5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0beeeaf5fb80b9a93e95f8667e620ecf.js"
+  "/soroll/precache-manifest.9293e4c7b253c8020c305700228e554f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/soroll/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
